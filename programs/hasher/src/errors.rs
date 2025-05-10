da5bc04abdcde5d2179644d19a5e0c20e3907b41
vibhumeh <vibhumeh1@gmail.com>
2025-05-11 01:55:03 +0530

    unstable --- pvkey auth 1

---
+++ programs/hasher/src/errors.rs
@@ -0,0 +1,16 @@
+// filepath: /Users/vibhu/rustycow/solonana/bootcamp/myapp/hasher/hasher/programs/hasher/src/errors.rs
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Missing Ed25519 instruction")]
+    MissingEd25519Instruction,
+    #[msg("Invalid Ed25519 instruction")]
+    InvalidEd25519Instruction,
+    #[msg("Invalid public key")]
+    InvalidPublicKey,
+    #[msg("Invalid message")]
+    InvalidMessage,
+    #[msg("Invalid signature")]
+    InvalidSignature,
+}
--- programs/hasher/src/lib.rs
+++ programs/hasher/src/lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
-
+use crate::errors::ErrorCode;
+use solana_program::sysvar::instructions::{load_instruction_at_checked, load_current_index_checked};
 declare_id!("EbRPnJaaBXkbur5nPB9BTfSf3w8FbiYnJQDAgmp78Esx");
 
 #[program]
@@ -23,6 +24,68 @@ pub mod hasher {
         //msg!("counter hash_id: {}",counter.hash_id);
         Ok(())
     }
+}   
+pub fn verify_ed25519_instruction(
+    instruction_sysvar: &AccountInfo,
+    expected_public_key: &[u8],
+    message: &[u8],
+    signature: &[u8]
+) -> Result<()> {
+    let current_index = load_current_index_checked(instruction_sysvar)?;
+    if current_index == 0 {
+        return Err(ErrorCode::MissingEd25519Instruction.into());
+    }
+
+    let ed25519_instruction = load_instruction_at_checked((current_index - 1) as usize, instruction_sysvar)?;
+    
+    // Verify the content of the Ed25519 instruction
+    let instruction_data = ed25519_instruction.data;
+    if instruction_data.len() < 2 {
+        return Err(ErrorCode::InvalidEd25519Instruction.into());
+    }
+
+    let num_signatures = instruction_data[0];
+    if num_signatures != 1 {
+        return Err(ErrorCode::InvalidEd25519Instruction.into());
+    }
+
+    // Parse Ed25519SignatureOffsets
+    let offsets: Ed25519SignatureOffsets = Ed25519SignatureOffsets::try_from_slice(&instruction_data[2..16])?;
+
+    // Verify public key
+    let pubkey_start = offsets.public_key_offset as usize;
+    let pubkey_end = pubkey_start + 32;
+    if &instruction_data[pubkey_start..pubkey_end] != expected_public_key {
+        return Err(ErrorCode::InvalidPublicKey.into());
+    }
+
+    // Verify message
+    let msg_start = offsets.message_data_offset as usize;
+    let msg_end = msg_start + offsets.message_data_size as usize;
+    if &instruction_data[msg_start..msg_end] != message {
+        return Err(ErrorCode::InvalidMessage.into());
+    }
+
+    // Verify signature
+    let sig_start = offsets.signature_offset as usize;
+    let sig_end = sig_start + 64;
+    if &instruction_data[sig_start..sig_end] != signature {
+        return Err(ErrorCode::InvalidSignature.into());
+    }
+
+    Ok(())
+}
+
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+struct Ed25519SignatureOffsets {
+    signature_offset: u16,
+    signature_instruction_index: u16,
+    public_key_offset: u16,
+    public_key_instruction_index: u16,
+    message_data_offset: u16,
+    message_data_size: u16,
+    message_instruction_index: u16,
 }
 
 #[derive(Accounts)]
